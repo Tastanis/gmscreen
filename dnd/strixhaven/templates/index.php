a91b32b0e3d367eda55514994fd5d8de65a60c00
User <user@localhost>
2025-08-04 15:37:13 -0700

    template backup data button

--- dnd/strixhaven/templates/index.php
+++ dnd/strixhaven/templates/index.php
@@ -73,6 +73,14 @@ $templatesData = loadTemplatesData();
                 <div class="sidebar-footer">
                     <button class="add-folder-btn" onclick="addNewFolder()">+ Add Folder</button>
                     <button class="delete-mode-btn" id="delete-mode-btn" onclick="toggleDeleteMode()">Delete Mode</button>
+                    <button class="data-recovery-btn" onclick="window.location.href='templates-recovery.php'">
+                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                            <polyline points="17,8 12,3 7,8"></polyline>
+                            <line x1="12" y1="3" x2="12" y2="15"></line>
+                        </svg>
+                        Data Recovery
+                    </button>
                 </div>
             <?php endif; ?>
         </div>
